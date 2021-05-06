db339ffe0e17482be07089edaeba4f5ddce09b4f
Renato Mertz De Olivera <rmertz@br0c02f74b5md6m.ml.com>
2021-05-06 19:14:02 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+import { nanoid } from 'nanoid';
+
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+import Todo from './components/Todo';
+
+function App(props) {
+
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if(id === task.id) {
+        return {...task, completed: !task.completed};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      if(id === task.id) {
+        return {...task, name: newName};
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  const taskList = tasks.map(task => (
+    <Todo 
+      id={task.id} 
+      name={task.name} 
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+  function addTask(name) {
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+      </div>
+      <h2 id="list-heading"> {headingText} </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
